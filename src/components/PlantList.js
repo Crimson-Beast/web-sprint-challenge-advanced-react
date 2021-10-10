800d968326d6ea159b6a488a35e09903cb84be2a
Daniel <dek.kneale@gmail.com>
2021-10-10 13:34:59 -0700

    plants list rendering with state on component mount

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+
 export default class PlantList extends Component {
+  
   // add state with a property called "plants" - initialize as an empty array
-  state = {
-    plants:[]
+  constructor() {
+    super();
+    this.state = {
+      plants: []
   }
+}
   // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    //   - fetch data from the server endpoint - http://localhost:3333/plants
+    axios.get('http://localhost:3333/plants').then(res =>{
+        //   - set the returned plants array to this.state.plants
+      this.setState({ plants: res.data})
+      console.log(this.state.plants)
+    })
+  }
+
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
